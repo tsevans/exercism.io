23b74ab13065474cf3d1f4bc1c753181a61bdf6a
Tom Evans <tsevans@vt.edu>
2018-02-01 15:09:13 -0500

    Fetched and completed hello world exercise... pretty simple

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting is an exercism exercis for the basic hello world program.
+package greeting
+
+// HelloWorld returns the string "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
