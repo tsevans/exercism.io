246ab7d028cc1cc35afffe34d7d0dbcf9a28280a
Tom Evans <tsevans@vt.edu>
2018-02-01 14:15:09 -0500

    Passed Bob exercise for Go

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,46 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob is an exercism exercise to simulate interactions with a lackadaisical teenager named Bob
 package bob
 
-// Hey should have a comment documenting it.
+import "strings"
+import "unicode"
+
+// Hey takes a phrase as input and returns Bob's response to it.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	//Clean up input string
+	remark = strings.TrimSpace(remark)
+
+	if remark == "" {
+		return "Fine. Be that way!"
+	}
+
+	var question bool = strings.HasSuffix(remark, "?")
+	var yell bool = allUppercase(remark)
+
+	if question && yell {
+		return "Calm down, I know what I'm doing!"
+	} else if question {
+		return "Sure."
+	} else if yell {
+		return "Whoa, chill out!"
+	}
+
+	return "Whatever."
+}
+
+// allUppercase checks if all letters in remark are uppercase
+func allUppercase(remark string) bool {
+	var hasLetter bool = false
+	var upcase bool = true
+	for _, x := range remark {
+		if unicode.IsLetter(x) {
+			hasLetter = true
+			if !unicode.IsUpper(x) {
+				upcase = false
+				break
+			}
+		}
+	}
+
+	return hasLetter && upcase
 }
